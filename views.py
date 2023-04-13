bf8f0f9bce6996a6f5847a9161fc17b2711bcb75
Ramazon2010 <127496992+ramazon00@users.noreply.github.com>
2023-04-13 14:49:30 +0500

    6_dars
    views core da

--- views.py
+++ views.py
@@ -1,27 +1,50 @@
-from django.views.generic import ListView, DetailView, TemplateView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Yangilik
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import UpdateView, DeleteView, CreateView, ListView
 from django.urls import reverse_lazy
+from .models import Post
+from django.contrib.auth.models import User
 
-class BlogListView(ListView):
-    model = Yangilik
-    template_name = 'home.html'
 
-class BlogDetailView(DetailView):
-    model = Yangilik
-    template_name = 'post_detail.html'
+# def home(request):
 
-class BlogCreateView(CreateView):
-    model = Yangilik
-    template_name = 'post_new.html'
-    fields = ['title', 'author', 'body']
+# 	all_posts = Post.newmanager.all()
 
-class BlogUpdateView(UpdateView):
-    model = Yangilik
-    template_name = 'post_edit.html'
-    fields = ['title', 'body']
+# Oldin bu bo'lgan
+# 	return render(request, 'home.html', {'posts' : all_posts})
 
-class BlogDeleteView(DeleteView):
-    model = Yangilik
-    template_name = 'post_delete.html'
-    succes_url = reverse_lazy('home')
+class home(ListView):
+	model = Post
+	template_name = 'home.html'
+
+
+def post_single(request, post):
+
+    post = get_object_or_404(Post, slug=post, status='published')
+    return render(request, 'single.html', {'post' : post})
+
+def user(request):
+
+    users = User.objects.all()
+
+
+    return render(request, 'user.html', {'posts' : users})
+
+class AddView(CreateView):
+	model = Post
+	template_name = 'add.html'
+	fields = "__all__"
+	succes_url = reverse_lazy('core:home')
+
+class EditView(UpdateView):
+	model = Post
+	template_name = 'edit.html'
+	fields = "__all__"
+	pk_url_kwarg = "pk"
+	succes_url = reverse_lazy('core:home')
+
+class Delete(DeleteView):
+	model = Post
+	pk_url_kwarg = "pk"
+	succes_url = reverse_lazy('core:home')
+	template_name = 'confirm-delete.html'
+
