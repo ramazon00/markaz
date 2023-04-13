426b091fc1d97fcfa634c87f3f27cee3f68a04cf
Ramazon2010 <127496992+ramazon00@users.noreply.github.com>
2023-04-13 14:50:31 +0500

    6_dars
    core/admin

--- admin.py
+++ admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
-from .models import Yangilik
-# Register your models here.
-admin.site.register(Yangilik)
+from .models import Post
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'id', 'image', 'slug', 'author', 'status_color')
+    prepopulated_fields = {'slug': ('title',)}
+
+
+admin.site.register(Post, AuthorAdmin)
