27ee4ad22644c60345a74fa5704e3ddeee74d44f
Ramazon2010 <127496992+ramazon00@users.noreply.github.com>
2023-04-15 14:30:06 +0500

    6_dars
    login/admin

--- admin.py
+++ admin.py
@@ -1,9 +1,3 @@
 from django.contrib import admin
-from .models import Post
 
-class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('title', 'status', 'id', 'image', 'slug', 'author', 'status_color')
-    prepopulated_fields = {'slug': ('title',)}
-
-
-admin.site.register(Post, AuthorAdmin)
+# Register your models here.
