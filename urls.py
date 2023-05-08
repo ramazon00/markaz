631ea6daa30279d962ee7664f37f286edc58c9a6
Ramazon2010 <127496992+ramazon00@users.noreply.github.com>
2023-05-08 20:19:32 +0500

    9_dars
    config

--- urls.py
+++ urls.py
@@ -1,12 +1,18 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
-from . import views
-
-app_name = 'login'
-
-
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
-    path("login/", auth_views.LoginView.as_view(), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-    path('register/', views.register_user, name="register"),
+    path('account/', include('django.contrib.auth.urls')),
+    path('', include('accounts.urls')),
+    path('account/', include('login.urls', namespace='login')),
+    path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('admin/', admin.site.urls),
+    path('articles/', include('articles.urls')),
 ]
+# O'chirdim
+#    path('accounts/', include('accounts.urls')),
+#   path('accounts/', include('django.contrib.auth.urls')),
+#    path('', include('pages.urls')),    
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
