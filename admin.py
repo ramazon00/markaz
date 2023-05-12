87a756681d6c501d9b058d4b22eb6fbdd6742669
Ramazon2010 <127496992+ramazon00@users.noreply.github.com>
2023-05-12 14:22:59 +0500

    9_dars
    articles

--- admin.py
+++ admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class CommentInLine(admin.TabularInline):
+    model = Comment
+    extra = 0
+
+class ArticleAdmin(admin.ModelAdmin):
+    inlines = [CommentInLine]
+
+admin.site.register(Video)
+admin.site.register(Music)
+admin.site.register(Article, ArticleAdmin)
+admin.site.register(Comment)
